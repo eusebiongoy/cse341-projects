15857cbeac7fbed74f489ddebdd6fc5011ec8b9a
eusebiongoy <banzaeusebio2@gmail.com>
2026-06-28 12:28:57 +0200

    get method

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const connectDB = require("../db/connect");
+const { ObjectId } = require("mongodb");
 
+// GET ALL contacts
 router.get("/", async (req, res) => {
   try {
     const db = await connectDB();
     const contacts = await db.collection("contacts").find().toArray();
     res.json(contacts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+});
+
+// GET ONE contact by ID (query parameter)
+router.get("/single", async (req, res) => {
+  try {
+    const db = await connectDB();
+
+    const id = req.query.id;
+
+    const contact = await db.collection("contacts").findOne({
+      _id: new ObjectId(id)
+    });
+
+    if (!contact) {
+      return res.status(404).send("Contact not found");
+    }
+
+    res.json(contact);
   } catch (err) {
     console.error(err);
     res.status(500).send("Server Error");
